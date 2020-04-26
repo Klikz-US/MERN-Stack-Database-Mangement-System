e25eef8d55cf6dad2a6077fec9c054f619ff8e60
Dewayne Johnson <dev@klikz.us>
2020-04-26 12:18:42 -0400

    Init Front Routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, {Component} from "react";
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
-import MicrochipList from "microchip-list.component";
-import EditMicrochip from "edit-microchip.component";
-import CreateMicrochip from "create-microchip.component";
+import MicrochipList from "./components/list-microchip.component";
+import EditMicrochip from "./components/edit-microchip.component";
+import RegisterMicrochip from "./components/register-microchip.component";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import logo from "./logo.png";
 
 class App extends Component {
   render() {
     return (
       <Router>
+        <header>
+          <nav className="container-fluid navbar navbar-expand-lg navbar-light bg-light">
+
+            <Link to="/" className="nav-link">
+              <img src={logo} width="auto" height="60" alt="STL Portal" />
+            </Link>
+
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">List Microchip</Link>
+                </li>
+
+                <li className="navbar-item">
+                  <Link to="/register" className="nav-link">Register Microchip</Link>
+                </li>
+
+              </ul>
+            </div>
+
+          </nav>
+        </header>
+
         <Route path = "/" exact component = {MicrochipList}></Route>
         <Route path = "/edit/:id" exact component = {EditMicrochip}></Route>
-        <Route path = "/create" exact component = {CreateMicrochip}></Route>
+        <Route path = "/register" exact component = {RegisterMicrochip}></Route>
       </Router>
     );
   }
---
+++ src/components/register-microchip.component.js
@@ -0,0 +1,80 @@
+import React, {Component} from "react";
+
+export default class RegisterMicrochip extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeMicrochipNumber = this.onChangeMicrochipNumber.bind(this);
+        this.onChangeOwnerEmail = this.onChangeOwnerEmail.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            microchip_Number: "",
+            owner_Email: "",
+        };
+    }
+
+    onChangeMicrochipNumber (e) {
+        this.setState({
+            microchip_Number: e.target.value
+        });
+    }
+
+    onChangeOwnerEmail (e) {
+        this.setState({
+            owner_Email: e.target.value
+        });
+    }
+
+    onSubmit (e) {
+        e.preventDefault();
+
+        this.setState({
+            microchip_Number: e.target.value,
+            owner_Email: e.target.value
+        });
+
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div className="register-microchip-container">
+                <h3>Register New Microchip</h3>
+
+                <form onSubmit={this.onSubmit}>
+
+                    <div className="form-group"> 
+                        <label htmlFor="microchipNumber">Microchip Number:</label>
+                        <input  type="text"
+                                className="form-control"
+                                id="microchipNumber"
+                                aria-describedby="microchipHelp"
+                                placeholder="Microchip Number"
+                                onChange={this.onChangeMicrochipNumber}
+                                />
+                        <small id="microchipHelp">Input 9, 10, 15 digits of Microchip Number</small>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="ownerEmail">Owner Email Address:</label>
+                        <input 
+                                type="email" 
+                                className="form-control"
+                                id="ownerEmail"
+                                aria-describedby="emailHelp"
+                                placeholder="Owner Email Address"
+                                onChange={this.onChangeOwnerEmail}
+                                />
+                        <small id="emailHelp">Input 9, 10, 15 digits of Microchip Number</small>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Register Microchip" className="btn btn-primary" />
+                    </div>
+
+                </form>
+            </div>
+        );
+    }
+}
