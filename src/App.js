afc9e24e3658cff2d0c62313085791a4d74eee06
Dewayne Johnson <dev@klikz.us>
2020-05-03 17:54:11 -0400

    Navigation DOM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import ListGroup from 'react-bootstrap/ListGroup'
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,6 +15,8 @@ import RegisterPet from "./components/register-pet.component";
 import OwnerList from "./components/list-owner.component";
 import EditOwner from "./components/update-owner.component";
 import RegisterOwner from "./components/register-owner.component";
+
+import ImportPet from "./components/import-pet.component";
 
 class App extends Component {
     constructor(props) {
@@ -34,24 +35,41 @@ class App extends Component {
                 <header>
                     <Navbar collapseOnSelect expand="lg" bg="info" variant="dark" className="shadow p-0 text-white">
                         <Container>
-                            <Navbar.Brand href="/">
-                                <img src={logo} width="auto" height="80" alt="STL Portal" />
+                            <Navbar.Brand>
+                                <Link to="/">
+                                    <img src={logo} width="auto" height="80" alt="STL Portal" />
+                                </Link>
                             </Navbar.Brand>
 
                             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                             <Navbar.Collapse id="responsive-navbar-nav">
 
                                 <Nav className="mr-auto" variant="pills">
+
                                     <NavDropdown title="Manage Pets">
-                                        <NavDropdown.Item href="/pets">Registered Pets</NavDropdown.Item>
-                                        <NavDropdown.Item href="/pets/register">Register New Pet</NavDropdown.Item>
+
+                                        <NavDropdown.Item>
+                                            <Link to="/pets">Registered Pets</Link>
+                                        </NavDropdown.Item>
+
+                                        <NavDropdown.Item>
+                                            <Link to="/pets/register">Register New Pet</Link>
+                                        </NavDropdown.Item>
+
                                         <NavDropdown.Divider />
                                         <NavDropdown.Item href="/pets/import">Import Pets</NavDropdown.Item>
                                         <NavDropdown.Item href="/pets/export">Export Pets</NavDropdown.Item>
                                     </NavDropdown>
-                                    <NavDropdown title="Manage Users">
-                                        <NavDropdown.Item href="/owners">Registered Owners</NavDropdown.Item>
-                                        <NavDropdown.Item href="/owners/register">Add New Owner</NavDropdown.Item>
+                                    <NavDropdown title="Manage Owners">
+
+                                        <NavDropdown.Item>
+                                            <Link to="/owners">Registered Owners</Link>
+                                        </NavDropdown.Item>
+
+                                        <NavDropdown.Item>
+                                            <Link to="/owners/register">Add New Owner</Link>
+                                        </NavDropdown.Item>
+
                                         <NavDropdown.Divider />
                                         <NavDropdown.Item href="/owners/import">Import Owners</NavDropdown.Item>
                                         <NavDropdown.Item href="/owners/export">Export Owners</NavDropdown.Item>
@@ -65,8 +83,15 @@ class App extends Component {
 
                                 <Nav variant="pills">
                                     <NavDropdown title="Account">
-                                        <NavDropdown.Item href="/account">Settings</NavDropdown.Item>
-                                        <NavDropdown.Item href="/account/logout">Logout</NavDropdown.Item>
+
+                                        <NavDropdown.Item>
+                                            <Link to="/account/settings">Settings</Link>
+                                        </NavDropdown.Item>
+
+                                        <NavDropdown.Item>
+                                            <Link to="/account/logout">Logout</Link>
+                                        </NavDropdown.Item>
+
                                     </NavDropdown>
                                 </Nav>
 
@@ -82,28 +107,28 @@ class App extends Component {
                     <Route path="/owners" exact component={OwnerList}></Route>
                     <Route path="/owners/edit/:id" component={EditOwner}></Route>
                     <Route path="/owners/register" component={RegisterOwner}></Route>
+
+                    <Route path="/pets/import" component={ImportPet}></Route>
                 </main>
 
                 <footer className="mt-5 pt-4 pb-4">
                     <Container>
                         <Row>
-                            <Col>
-                                <img src={logo} width="100%" height="auto" alt="STL Portal" />
+                            <Col className="text-center">
+                                <img src={logo} width="60%" height="auto" alt="STL Portal" />
                             </Col>
 
-                            <Col className="border border-light border-top-0 border-bottom-0 pb-2 mb-4">
-                                <h4 className="text-white p-2">Quick Links</h4>
-                                <ListGroup>
-                                    <ListGroup.Item action variant="primary" href="https://shop.savethislife.com">Save This Life Shopify</ListGroup.Item>
-                                    <ListGroup.Item action variant="success" href="https://shop.savethislife.com">Save This Life Brand</ListGroup.Item>
-                                    <ListGroup.Item action variant="danger" href="https://shop.savethislife.com">Asana Dashboard</ListGroup.Item>
-                                    <ListGroup.Item action variant="warning" href="https://shop.savethislife.com">Talkdesk</ListGroup.Item>
-                                    <ListGroup.Item action variant="info" href="https://shop.savethislife.com">Resources</ListGroup.Item>
-                                </ListGroup>
+                            <Col className="border border-dark border-top-0 border-bottom-0 pb-2 mb-4">
+                                <h4 className="text-dark p-2">Quick Links</h4>
+                                <Nav.Link className="text-info" href="https://shop.savethislife.com" target="_blank">Save This Life Shopify</Nav.Link>
+                                <Nav.Link className="text-danger" href="https://shop.savethislife.com" target="_blank">Asana Dashboard</Nav.Link>
+                                <Nav.Link className="text-alert" href="https://shop.savethislife.com" target="_blank">TalkDesk</Nav.Link>
+                                <Nav.Link className="text-success" href="https://shop.savethislife.com" target="_blank">CSR Portal</Nav.Link>
+                                <Nav.Link className="text-primary" href="https://shop.savethislife.com" target="_blank">Resources</Nav.Link>
                             </Col>
 
                             <Col>
-                                <h4 className="text-white p-2">Live Updates</h4>
+                                <h4 className="text-dark p-2">Live Updates</h4>
                             </Col>
 
                         </Row>
---
+++ src/components/import-pet.component.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Container } from "react-bootstrap";
+
+export default class ImportPet extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <Container>
+
+            </Container>
+        );
+    }
+}
